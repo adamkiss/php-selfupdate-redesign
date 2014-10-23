ccb7cd2abbe59cbae4870ba894727febee61923d
Adam Kiss <iam@adamkiss.com>
2014-10-23 02:27:12 +0200

    Debugging hooks brb

--- hookdebug.php
+++ hookdebug.php
@@ -31,7 +31,7 @@
   <div class="wrapper">
     <h1><?= date('d M Y H:i:s'); ?></h1>
     <pre><?php
-      var_dump($_POST);
+      var_dump($_REQUEST['payload']);
 
       if (!empty($_POST['payload'])) {
         $payload = json_decode($_POST['payload']);
