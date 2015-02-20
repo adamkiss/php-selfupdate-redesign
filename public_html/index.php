db173fea40b535e0d1477b92c7670106c63479f1
Adam Kiss <iam@adamkiss.com>
2015-02-20 12:56:41 +0100

    Add page loading and basic 'what to do' structure to index.php

--- public_html/index.php
+++ public_html/index.php
@@ -2,11 +2,32 @@
 
   $request = trim($_GET['request']);
 
-  $use_cache = false;
+  $use_cache = file_exists('../.bypass_cache');
   $cached = false;
 
-  if ($use_cache && $cached){
+  var_dump($use_cache);
 
+  if ($use_cache && $cached){
+    // Load Cache
   }else{
-    echo " Hello. This is v0.1. You requested: {$_GET['req']}";
+    echo " Hello. This is v0.1. You requested: {$request}";
+
+    // Load URL
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "http://processwire.com/{$request}");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $pw_page = curl_exec($ch);
+    curl_close($ch);
+
+    $pw_page_code = htmlentities($pw_page);
+
+    echo "<pre>{$pw_page_code}</pre>";
+
+    // Be nice (clean up tracking code)
+
+    // Add custom CSS/JS
+
+    // Load any custom modification from ../pages/*
+
+    // Cache file
   }
