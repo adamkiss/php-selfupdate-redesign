7ed756728fcd6d9a9a74e26170e25f8c0727ecc0
Adam Kiss <iam@adamkiss.com>
2015-02-20 12:40:23 +0100

    Added .htaccess and very basic index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,3 +1,12 @@
 <?php
 
-  echo "Whaaaat! It works. And it updates.";
+  $request = trim($_GET['request']);
+
+  $use_cache = false;
+  $cached = false;
+
+  if ($use_cache && $cached){
+
+  }else{
+    echo " Hello. This is v0.1. You requested: {$_GET['req']}";
+  }
